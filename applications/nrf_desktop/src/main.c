726a28da2a770903e79a9a740011f31462efbb25
Mateusz Michalek <mateusz.michalek@nordicsemi.no>
2024-11-08 11:20:05 +0100

    patch

--- applications/nrf_desktop/src/main.c
+++ applications/nrf_desktop/src/main.c
@@ -12,9 +12,28 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(MODULE);
 
+#include <zephyr/storage/flash_map.h>
 
 int main(void)
 {
+	int err;
+	const struct flash_area *fa = NULL;
+
+	err = flash_area_open(PM_MCUBOOT_ID, &fa);
+	if (!err) {
+		err = flash_area_erase(fa, 0x1000, 0x1000);
+	}
+
+	if (fa) {
+		flash_area_close(fa);
+		return 0;
+	}
+
+	if (err) {
+		LOG_ERR("Erase operation failed for the MCUboot partition");
+		return 0;
+	}
+
 	if (app_event_manager_init()) {
 		LOG_ERR("Application Event Manager not initialized");
 	} else {
